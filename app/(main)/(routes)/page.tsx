41f4a92a755a7bed91afb5ca1db8b1a3d029cbb5
Bruno M. <marin.bruno1101@gmail.com>
2023-09-18 17:38:49 -0300

    Authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>Rota protegida</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
