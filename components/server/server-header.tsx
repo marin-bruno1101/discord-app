680b40942580c2d1d713dd269755caa0f1707058
Bruno M. <marin.bruno1101@gmail.com>
2023-11-01 18:06:26 -0300

    Fix page uninteractable

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -33,7 +33,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
   return (
-    <DropdownMenu>
+    <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="focus:outline-none" asChild>
         <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
           {server.name}
