161e2b32a0c00ad03c53bc9177c78dbe63559b9e
Bruno M. <marin.bruno1101@gmail.com>
2023-11-17 17:24:45 -0300

    chat-messages status aligned to center

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import { Member, Message, Profile } from "@prisma/client";
-import { Loader2, ServerCrash } from "lucide-react";
-import { Fragment, useRef, ElementRef } from "react";
 import { format } from "date-fns";
+import { Loader2, ServerCrash } from "lucide-react";
+import { ElementRef, Fragment, useRef } from "react";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
-
+import { useChatScroll } from "@/hooks/use-chat-scroll";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 import { ChatItem } from "./chat-item";
 import { ChatWelcome } from "./chat-welcome";
-import { useChatSocket } from "@/hooks/use-chat-socket";
-import { useChatScroll } from "@/hooks/use-chat-scroll";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -65,7 +64,7 @@ export const ChatMessages = ({
 
   if (status === "loading") {
     return (
-      <div className="flex flex-col flex-1 justify-center">
+      <div className="flex flex-col flex-1 justify-center items-center">
         <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
         <p className="text-xs text-zinc-500 dark:text-zinc-400">
           Loading messages...
@@ -76,7 +75,7 @@ export const ChatMessages = ({
 
   if (status === "error") {
     return (
-      <div className="flex flex-col flex-1 justify-center">
+      <div className="flex flex-col flex-1 justify-center items-center">
         <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
         <p className="text-xs text-zinc-500 dark:text-zinc-400">
           Something went wrong
